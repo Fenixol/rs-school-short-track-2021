ed8e4f4e035600098fc3af982b46987c90635e6c
fenixoleg7 <fenixoleg7@gmail.com>
2021-04-09 14:42:35 +0300

    feat: add solution for 01 task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,36 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(arr) {
+  let sum = 0;
+  for (let j = 0; j < arr.length; j++) {
+    let first = 0;
+    if (j > 0) {
+      if (!arr[j].includes(0) && arr[j].length > 1) {
+        for (let n = 0; n < arr[j].length; n++) {
+          sum += arr[j][n];
+        }
+      } else {
+        // eslint-disable-next-line no-lonely-if
+        if (arr[j].length === 1) {
+          first = arr[j - 1];
+          if (first > 0) {
+            sum += Number(arr[j]);
+          }
+        } else {
+          first = arr[j - 1][j];
+          if (first > 0) {
+            sum += arr[j][j];
+          }
+        }
+      }
+    } else {
+      for (let n = 0; n < arr[j].length; n++) {
+        sum += arr[j][n];
+      }
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
